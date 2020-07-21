94b53a33ee03df6fe204c7b5fc91df04f3660b60
Charlie <porthose.cjsmo@gmail.com>
2020-07-21 00:13:55 -0700

    remove comments

--- ampnado/ampnado.py
+++ ampnado/ampnado.py
@@ -58,9 +58,9 @@ class SetUp():
 		FJ.globstuff()
 
 		#Not needed 
-		# picdics = FJ.PicDics
-		# print(picdics)
-# 		Data().tags_update_artID(picdics)
+		picdics = FJ.PicDics
+		print(picdics)
+		Data().tags_update_artID(picdics)
 
 
 		btime = time.time()
@@ -73,51 +73,51 @@ class SetUp():
 		artidtime = ctime - atime
 		print("AddArtistId time %s" % artidtime)
 
-# 		from functions import AddAlbumId
-# 		AddAlbumId().add_albumids()
-# 		dtime = time.time()
-# 		albidtime = dtime - atime
-# 		print("AddAlbumId time %s" % albidtime)
+		from functions import AddAlbumId
+		AddAlbumId().add_albumids()
+		dtime = time.time()
+		albidtime = dtime - atime
+		print("AddAlbumId time %s" % albidtime)
 
-# 		from artistview import ArtistView
-# 		from artistview import ArtistChunkIt
-# 		AV = ArtistView().main()
-# 		ArtistChunkIt().main(AV, os.environ["AMP_OFFSET_SIZE"])
-# 		etime = time.time()
-# 		artistviewtime = etime - atime
-# 		print("Artistview time %s" % artistviewtime)		
+		from artistview import ArtistView
+		from artistview import ArtistChunkIt
+		AV = ArtistView().main()
+		ArtistChunkIt().main(AV, os.environ["AMP_OFFSET_SIZE"])
+		etime = time.time()
+		artistviewtime = etime - atime
+		print("Artistview time %s" % artistviewtime)		
 
-# 		from albumview import AlbumView
-# 		from albumview import AlbumChunkIt
-# 		ALBV = AlbumView().main()
-# 		AlbumChunkIt().main(ALBV, os.environ["AMP_OFFSET_SIZE"])
-# 		ftime = time.time()
-# 		albviewtime = ftime - atime
-# 		print("Albumview time %s" % albviewtime)		
+		from albumview import AlbumView
+		ALBV = AlbumView().main()
+		from albumview import AlbumChunkIt
+		AlbumChunkIt().main(ALBV, os.environ["AMP_OFFSET_SIZE"])
+		ftime = time.time()
+		albviewtime = ftime - atime
+		print("Albumview time %s" % albviewtime)		
 
-# 		from songview import SongView
-# 		SongView().create_songView_db(os.environ["AMP_OFFSET_SIZE"])
-# 		gtime = time.time()
-# 		songviewtime = gtime - atime
-# 		print("Songview time %s" % songviewtime)
+		from songview import SongView
+		SongView().create_songView_db(os.environ["AMP_OFFSET_SIZE"])
+		gtime = time.time()
+		songviewtime = gtime - atime
+		print("Songview time %s" % songviewtime)
 		
-# 		from functions import Indexes
-# 		Indexes().creat_db_indexes()
-# 		htime = time.time()
-# 		indextime = htime - atime
-# 		print("Index time %s" % indextime)
+		from functions import Indexes
+		Indexes().creat_db_indexes()
+		htime = time.time()
+		indextime = htime - atime
+		print("Index time %s" % indextime)
 		
-# 		from functions import DbStats
-# 		DbStats().db_stats()
-# 		itime = time.time()
-# 		statstime = itime - atime
-# 		print("DBStats time is %s" % statstime)
+		from functions import DbStats
+		DbStats().db_stats()
+		itime = time.time()
+		statstime = itime - atime
+		print("DBStats time is %s" % statstime)
 
-# 		from functions import RandomArtDb
-# 		RandomArtDb().create_random_art_db()
-# 		jtime = time.time()
-# 		ranarttime = jtime - atime
-# 		print("RandomArtDB time is %s" % ranarttime)
+		from functions import RandomArtDb
+		RandomArtDb().create_random_art_db()
+		jtime = time.time()
+		ranarttime = jtime - atime
+		print("RandomArtDB time is %s" % ranarttime)
 
 
 
